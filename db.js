db5c20896f64a0955e8953d0a985d66d3b747594
Admin <admin@Admins-MacBook-Pro-7.local>
2026-09-17 11:43:08 +0200

    use DATABASE_URL when available

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'arab_db',
-  password: '4556',
-  port: 5432,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // Render Postgres requires SSL
+    })
+  : new Pool({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'arab_db',
+      password: '4556',
+      port: 5432,
+    });
 
 module.exports = pool;
